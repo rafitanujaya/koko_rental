8244ec978435782defc3234970b8eb914f07336c
wolf33b <wolf33b@duck.com>
2025-02-08 00:53:43 +0700

    feat: fix another naming to follow Style Guide, and adding remove data

--- abi.py
+++ abi.py
@@ -7,7 +7,6 @@ int_jumlah_maks = 10
 percobaan = 0
 batas_percobaan = 3
 # array
-array_indeks = [0] * int_jumlah_maks
 array_brand = ['/'] * int_jumlah_maks
 array_model = ['/'] * int_jumlah_maks
 array_price_rental = [0] * int_jumlah_maks
@@ -17,9 +16,7 @@ array_phone_supir = [0] * int_jumlah_maks
 
 #FOR DEBUGGING PURPOSE
 def DEBUG_DUMMY_DATA():
-    global array_indeks, array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir
-
-    array_indeks = list(range(1, 11))
+    global array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir
     array_brand = ['1Toyota', '2Honda', '3Suzuki', '4Daihatsu', '5Nissan', '6Mitsubishi', '7Mazda', '8Kia', '9Hyundai', '10Ford']
     array_model = ['Avanza', 'Civic', 'Ertiga', 'Xenia', 'Livina', 'Pajero', 'CX-5', 'Seltos', 'Elantra', 'Focus']
     array_price_rental = [200000000, 300000000, 150000000, 180000000, 250000000, 400000000, 350000000, 220000000, 270000000, 320000000]
@@ -48,7 +45,7 @@ def fungsi_tambah_array(indeks, teks, array_nama):
         array_nama[indeks] = massukkan_pengguna
     return array_nama[indeks]
 
-def fungsi_tampil_kendaraan(int_jumlah_masuk, array_indeks, array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir):
+def fungsi_tampil_kendaraan(int_jumlah_masuk, array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir):
     fungsi_bersihkan_layar()
     print('Tampilan data kendaraan:')
     print('------------------------')
@@ -57,7 +54,6 @@ def fungsi_tampil_kendaraan(int_jumlah_masuk, array_indeks, array_brand, array_m
         print('Tidak ada kendaraan yang ditambahkan.')
     else:
         for i in range(int_jumlah_masuk):  # Mulai dari 0
-            print(f'indeks: {array_indeks[i]}')
             print(f'Brand: {array_brand[i]}')
             print(f'Model: {array_model[i]}')
             print(f'Harga: {array_price_rental[i]}')
@@ -136,7 +132,7 @@ def subrutin_menu_utama(menu_pilihan, int_jumlah_masuk):
             case 2:
                 subrutin_cari_kendaraan()
             case 3:
-                fungsi_tampil_kendaraan(int_jumlah_masuk, array_indeks, array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir)
+                fungsi_tampil_kendaraan(int_jumlah_masuk, array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir)
             case 99:
                 int_jumlah_masuk = DEBUG_DUMMY_DATA()  # THIS IS DEBUG!
             case 0:
@@ -168,7 +164,7 @@ def subrutin_menu_direksi(menu_pilihan,int_jumlah_masuk):
             case 1:
                 int_jumlah_masuk = subrutin_tambah_kendaraan(int_jumlah_maks, int_jumlah_masuk)  # Update indeks terakhir
             case 2:
-                subrutin_hapus_kendaraan()
+                subrutin_hapus_kendaraan(array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir)
             case 3:
                 subrutin_urutkan_kendaraan()
             case 4:
@@ -212,12 +208,11 @@ def subrutin_tambah_kendaraan(int_jumlah_maks, int_jumlah_masuk):
             print('#-------------------------------------------------#')
         jumlah_kendaraan = int(input('Masukkan jumlah kendaraan: '))
 
-    for i in range(jumlah_kendaraan):  # Mulai dari 0
+    for i in range(jumlah_kendaraan):
         fungsi_bersihkan_layar()
         print(f'#------------------------------------------#')
         print(f'|    Masukkan data kendaraan ke-{i+1}      |')
         print(f'#------------------------------------------#')
-        fungsi_tambah_array(int_jumlah_masuk + i, 'indeks', array_indeks)
         fungsi_tambah_array(int_jumlah_masuk + i, 'brand', array_brand)
         fungsi_tambah_array(int_jumlah_masuk + i, 'model', array_model)
         fungsi_tambah_array(int_jumlah_masuk + i, 'harga', array_price_rental)
@@ -228,10 +223,35 @@ def subrutin_tambah_kendaraan(int_jumlah_maks, int_jumlah_masuk):
     return int_jumlah_masuk + jumlah_kendaraan
 
 # subrutin hapus kendaraan
-def subrutin_hapus_kendaraan():
-    fungsi_bersihkan_layar()
-    print('Hapus Kendaraan')
-    input('Tekan Enter untuk Melanjutkan')
+def subrutin_hapus_kendaraan(array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir):
+    indeks_dihapus = int(input('Masukkan indeks kendaraan yang ingin dihapus: '))-1
+    if 0 <= indeks_dihapus < len(array_brand):
+        print(f'Menghapus kendaraan dengan data:')
+        print(f'Brand: {array_brand[indeks_dihapus]}')
+        print(f'Model: {array_model[indeks_dihapus]}')
+        print(f'Harga Rental: {array_price_rental[indeks_dihapus]}')
+        print(f'Plat Nomor: {array_plat_nomor[indeks_dihapus]}')
+        print(f'Supir: {array_supir[indeks_dihapus]}')
+        print(f'Phone Supir: {array_phone_supir[indeks_dihapus]}')
+
+        for i in range(indeks_dihapus, len(array_brand) - 1):
+            array_brand[i] = array_brand[i + 1]
+            array_model[i] = array_model[i + 1]
+            array_price_rental[i] = array_price_rental[i + 1]
+            array_plat_nomor[i] = array_plat_nomor[i + 1]
+            array_supir[i] = array_supir[i + 1]
+            array_phone_supir[i] = array_phone_supir[i + 1]
+
+        array_brand[-1] = '/'
+        array_model[-1] = '/'
+        array_price_rental[-1] = 0
+        array_plat_nomor[-1] = '/'
+        array_supir[-1] = '/'
+        array_phone_supir[-1] = 0
+
+        print('Kendaraan berhasil dihapus.')
+    else:
+        print('Indeks tidak valid.')
 
 # placeholder urutkan kendaraan
 def subrutin_urutkan_kendaraan():
