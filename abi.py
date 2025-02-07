1c8728f3bff82a735dc3264c30fe50e91c511e2a
wolf33b <wolf33b@duck.com>
2025-02-07 22:23:14 +0700

    feat: fix bug and add array logic

--- abi.py
+++ abi.py
@@ -1,29 +1,34 @@
 import os
 
-#kamus
-#int
+# kamus
+# int
 int_jumlah_input = 0
 int_jumlah_maks = 30
-#array
+# array
 array_index = [0] * int_jumlah_maks
-array_brand =  ['/'] * int_jumlah_maks
+array_brand = ['/'] * int_jumlah_maks
 array_model = ['/'] * int_jumlah_maks
 array_harga = [0] * int_jumlah_maks
-array_platnomor =  ['/'] * int_jumlah_maks
+array_platnomor = ['/'] * int_jumlah_maks
 array_supir = ['/'] * int_jumlah_maks
 array_supir_nomor = [0] * int_jumlah_maks
 
-#FUNGSI
+
+# FUNGSI
 def fun_bersihkan_layar():
-    #ini untuk bersihkan layar
+    # ini untuk bersihkan layar
     os.system('cls' if os.name == 'nt' else 'clear')
-def fun_tambah_array(indeks,teks,array_nama):
+
+
+def fun_tambah_array(indeks, teks, array_nama):
     if teks == 'indeks':
         temp = array_nama[indeks] = indeks
     else:
-        massukkan_pengguna = str(input('Masukkan '+teks+' : '))
+        massukkan_pengguna = str(input('Masukkan ' + teks + ' : '))
         temp = array_nama[indeks] = massukkan_pengguna
     return temp
+
+
 def fun_tampil_kendaraan(int_jumlah_input):
     print(int_jumlah_input)
     fun_bersihkan_layar()
@@ -42,10 +47,10 @@ def fun_tampil_kendaraan(int_jumlah_input):
     input('Tekan Enter untuk Melanjutkan')
 
 
-#PROSEDURE
-def seq_menu_utama(choice,int_jumlah_input):
+# PROSEDURE
+def seq_menu_utama(choice, int_jumlah_input):
     while choice != 0:
-        #placeholder untuk main menu
+        # placeholder untuk main menu
         fun_bersihkan_layar()
         print('Menu utama:')
         print('1. Tambah Kendaraan')
@@ -55,7 +60,7 @@ def seq_menu_utama(choice,int_jumlah_input):
         print('5. Tampil Kendaraan')
         print('0. Keluar')
         choice = int(input('Pilihan: '))
-        #pilih menu
+        # pilih menu
         match choice:
             case 1:
                 int_jumlah_input = seq_tambah_kendaraan(int_jumlah_maks)
@@ -70,11 +75,12 @@ def seq_menu_utama(choice,int_jumlah_input):
             case 0:
                 break
             case _:
-                #validasi
+                # validasi
                 print('Pilihan tidak valid. Silakan coba lagi.')
                 input('Tekan Enter untuk Melanjutkan')
 
-#sequential tambah kendaraan pada array
+
+# sequential tambah kendaraan pada array
 def seq_tambah_kendaraan(int_jumlah_maks):
     fun_bersihkan_layar()
     print('Tambah Kendaraan')
@@ -88,13 +94,13 @@ def seq_tambah_kendaraan(int_jumlah_maks):
         else:
             print('Jumlah melebihi batas maksimum. Harap masukkan ulang!')
         int_jumlah_input = int(input('Masukkan jumlah kendaraan: '))
-        
+
     for i in range(int_jumlah_input):
         fun_bersihkan_layar()
-        print(f'masukkan data kendaraan ke-{i+1}')
+        print(f'masukkan data kendaraan ke-{i + 1}')
         print('------------------------')
         fun_tambah_array(i, 'indeks', array_index)
-        fun_tambah_array(i,'brand',array_brand)
+        fun_tambah_array(i, 'brand', array_brand)
         fun_tambah_array(i, 'model', array_model)
         fun_tambah_array(i, 'harga', array_harga)
         fun_tambah_array(i, 'platnomor', array_platnomor)
@@ -103,26 +109,27 @@ def seq_tambah_kendaraan(int_jumlah_maks):
     return int_jumlah_input
 
 
-
-#placeholder hapus kendaraan
+# placeholder hapus kendaraan
 def seq_hapus_kendaraan():
     fun_bersihkan_layar()
     print('Hapus Kendaraan')
     input('Tekan Enter untuk Melanjutkan')
 
-#placeholder urutkan kendaraan
+
+# placeholder urutkan kendaraan
 def seq_urutkan_kendaraan():
     fun_bersihkan_layar()
     print('Urutkan Kendaraan')
     input('Tekan Enter untuk Melanjutkan')
 
-#placeholder cari kendaraan
+
+# placeholder cari kendaraan
 def seq_cari_kendaraan():
     fun_bersihkan_layar()
     print('Cari Kendaraan')
     input('Tekan Enter untuk Melanjutkan')
 
 
-#DEBUG PANGGIL FUNGSI
+# DEBUG PANGGIL FUNGSI
 if __name__ == '__main__':
-    seq_menu_utama(1,int_jumlah_input)
+    seq_menu_utama(1, int_jumlah_input)
