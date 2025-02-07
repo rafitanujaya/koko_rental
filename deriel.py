b25456929ff802b88024efb01d2c5c25e2d47825
Deril <derilhohoy552@gmail.com>
2025-02-07 21:03:47 +0700

    pilihan antara admin atau user

--- deriel.py
+++ deriel.py
@@ -1,1 +1,25 @@
 #  deriel
+import os
+from abi import *
+def menu_login():
+    print('<<<   login  >>>')
+    print('----------------')
+    print('1. Admin')
+    print('2. User')
+    print('0. Keluar')
+    print('----------------')
+    status = int(input('Pilihan anda: '))
+    while(status < 0) or (status > 2):
+        os.system('cls')
+        print('pilihan tidak ada')
+        os.system('pause')
+        print('<<<   login  >>>')
+        print('----------------')
+        print('1. Admin')
+        print('2. User')
+        print('0. Keluar')
+        print('----------------')
+        status = int(input('Pilihan anda: '))
+    return status
+
+posisi = menu_login()
