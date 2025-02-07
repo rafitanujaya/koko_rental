828a1ae0748135b3722d8c0fa864c94fbf3ea81a
rafi tanujaya <rafibussines@gmail.com>
2025-02-07 19:46:12 +0700

    feat: add ui and file member

--- development.py
+++ development.py
@@ -1,1 +1,27 @@
 # TEMPAT DEVELOPMENT
+
+# I.S :
+# F.S :
+
+# SUBRUTIN
+
+# FUNCTION
+
+def main():
+    print('=====================================')
+    print('|           KOKO RENTAL             |')
+    print('=====================================')
+    print('| Waktu DD-MM-YYY                   |')
+    print('|                                   |')
+    print('| Selamat datang di KOKO RENTAL!    |')
+    print('| Silahkan pilih peran Anda:        |')
+    print('|  1. Admin                         |')
+    print('|  2. User                          |')
+    print('|  0. Keluar                        |')
+    print('=====================================')
+    print('Masukkan pilihan Anda')
+    
+    
+    
+    
+main()
