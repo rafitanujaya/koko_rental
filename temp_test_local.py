3bdbe3305dd07556adeb7d94d40545a32437a5e5
wolf33b <wolf33b@duck.com>
2025-02-08 00:27:48 +0700

    feat: replace more naming to follow Style Guide, and adding new sub menu

--- temp_test_local.py
+++ temp_test_local.py
@@ -1,12 +1,8 @@
 import os
 
-# kamus
-# int
 int_jumlah_masuk = 0
 int_jumlah_maks = 10
-percobaan = 0
-batas_percobaan = 3
-# array
+
 array_indeks = [0] * int_jumlah_maks
 array_brand = ['/'] * int_jumlah_maks
 array_model = ['/'] * int_jumlah_maks
@@ -15,10 +11,8 @@ array_plat_nomor = ['/'] * int_jumlah_maks
 array_supir = ['/'] * int_jumlah_maks
 array_phone_supir = [0] * int_jumlah_maks
 
-#FOR DEBUGGING PURPOSE
 def DEBUG_DUMMY_DATA():
     global array_indeks, array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir
-
     array_indeks = list(range(1, 11))
     array_brand = ['1Toyota', '2Honda', '3Suzuki', '4Daihatsu', '5Nissan', '6Mitsubishi', '7Mazda', '8Kia', '9Hyundai', '10Ford']
     array_model = ['Avanza', 'Civic', 'Ertiga', 'Xenia', 'Livina', 'Pajero', 'CX-5', 'Seltos', 'Elantra', 'Focus']
@@ -28,21 +22,12 @@ def DEBUG_DUMMY_DATA():
     array_phone_supir = ['081234567890', '082345678901', '083456789012', '084567890123', '085678901234', '086789012345', '087890123456', '088901234567', '089012345678', '090123456789']
     return 10
 
-
-
-
-
-
-
-
-# FUNGSI
 def fungsi_bersihkan_layar():
-    # ini untuk bersihkan layar
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def fungsi_tambah_array(indeks, teks, array_nama):
     if teks == 'indeks':
-        array_nama[indeks] = indeks + 1  # Menyimpan indeks yang benar (indeks + 1)
+        array_nama[indeks] = indeks + 1
     else:
         massukkan_pengguna = str(input('Masukkan ' + teks + ' : '))
         array_nama[indeks] = massukkan_pengguna
@@ -56,7 +41,7 @@ def fungsi_tampil_kendaraan(int_jumlah_masuk, array_indeks, array_brand, array_m
     if int_jumlah_masuk == 0:
         print('Tidak ada kendaraan yang ditambahkan.')
     else:
-        for i in range(int_jumlah_masuk):  # Mulai dari 0
+        for i in range(int_jumlah_masuk):
             print(f'indeks: {array_indeks[i]}')
             print(f'Brand: {array_brand[i]}')
             print(f'Model: {array_model[i]}')
@@ -68,12 +53,10 @@ def fungsi_tampil_kendaraan(int_jumlah_masuk, array_indeks, array_brand, array_m
     input('Tekan Enter untuk Melanjutkan')
     return int_jumlah_masuk
 
-
-# PROSEDURE
-
-def subrutin_menu_login(percobaan, batas_percobaan,int_jumlah_masuk):
-
-    while percobaan < batas_percobaan:
+def subrutin_menu_login():
+    percobaan = 0
+    batas_percobaan = 3
+    while percobaan < batas_percobaan+1:
         fungsi_bersihkan_layar()
         print('#--selamat datang di koko rental--#')
         print('|                                 |')
@@ -102,9 +85,6 @@ def subrutin_menu_login(percobaan, batas_percobaan,int_jumlah_masuk):
 
         elif pilihan == '2':
             print('Login sebagai User berhasil!')
-            #PLACEHOLDER
-        elif pilihan == '0':
-            exit(0)
         else:
             print('Pilihan tidak valid. Silakan coba lagi.')
     fungsi_bersihkan_layar()
@@ -121,24 +101,29 @@ def subrutin_menu_utama(menu_pilihan, int_jumlah_masuk):
         fungsi_bersihkan_layar()
         print('#--selamat datang di koko rental--#')
         print('|                                 |')
-        print('| Menu Utama:                     |')
-        print('| 1. Direksi kendaraan            |')
-        print('| 2. Cari Kendaraan               |')
-        print('| 3. Tampil Kendaraan             |')
+        print('| 1. mengurus data kendaraan      |')
+        print('| 2. Hapus Kendaraan              |')
+        print('| 3. Urutkan Kendaraan            |')
+        print('| 4. Cari Kendaraan               |')
+        print('| 5. Tampil Kendaraan             |')
         print('| 0. Keluar                       |')
         print('|                                 |')
         print('#---------------------------------#')
         menu_pilihan = int(input('Pilihan: '))
-        # pilih menu
+
         match menu_pilihan:
             case 1:
-                int_jumlah_masuk = subrutin_menu_direksi(menu_pilihan,int_jumlah_masuk)
+                int_jumlah_masuk = subrutin_tambah_kendaraan(int_jumlah_maks, int_jumlah_masuk)
             case 2:
-                subrutin_cari_kendaraan()
+                subrutin_hapus_kendaraan()
             case 3:
+                subrutin_urutkan_kendaraan()
+            case 4:
+                subrutin_cari_kendaraan()
+            case 5:
                 fungsi_tampil_kendaraan(int_jumlah_masuk, array_indeks, array_brand, array_model, array_price_rental, array_plat_nomor, array_supir, array_phone_supir)
             case 99:
-                int_jumlah_masuk = DEBUG_DUMMY_DATA()  # THIS IS DEBUG!
+                int_jumlah_masuk = DEBUG_DUMMY_DATA()
             case 0:
                 break
             case _:
@@ -149,51 +134,11 @@ def subrutin_menu_utama(menu_pilihan, int_jumlah_masuk):
                 print('|                            /_______\\ |')
                 print('#--------------------------------------#')
 
-
-def subrutin_menu_direksi(menu_pilihan,int_jumlah_masuk):
-    while menu_pilihan != 0:
-        fungsi_bersihkan_layar()
-        print('#--selamat datang di koko rental--#')
-        print('|                                 |')
-        print('| Menu direksi kendaraan:         |')
-        print('| 1. Tambah Kendaraan             |')
-        print('| 2. Hapus Kendaraan              |')
-        print('| 3. Urutkan Kendaraan            |')
-        print('| 4. Cari Kendaraan               |')
-        print('| 0. Kembali                      |')
-        print('|                                 |')
-        print('#---------------------------------#')
-        menu_pilihan = int(input('Pilihan: '))
-        # pilih menu
-        match menu_pilihan:
-            case 1:
-                int_jumlah_masuk = subrutin_tambah_kendaraan(int_jumlah_maks, int_jumlah_masuk)  # Update indeks terakhir
-            case 2:
-                subrutin_hapus_kendaraan()
-            case 3:
-                subrutin_urutkan_kendaraan()
-            case 4:
-                subrutin_cari_kendaraan()
-            case 99:
-                int_jumlah_masuk = DEBUG_DUMMY_DATA()  # THIS IS DEBUG!
-            case 0:
-                return int_jumlah_masuk
-            case _:
-                print('#-< PERHATIAN >------------------------#')
-                print('|                               / \\    |')
-                print('|  NOMOR MENU TIDAK ADA!       /   \\   |')
-                print('|  HARAP ULANGIN KEMBALI      /  !  \\  |')
-                print('|                            /_______\\ |')
-                print('#--------------------------------------#')
-
-
-# subrutin tambah kendaraan pada array
 def subrutin_tambah_kendaraan(int_jumlah_maks, int_jumlah_masuk):
     fungsi_bersihkan_layar()
     print('#------------------------------------------#')
     print('|      Masukkan data kendaraan baru        |')
     print('#------------------------------------------#')
-    # Loop validasi
     jumlah_kendaraan = int(input('Masukkan jumlah kendaraan: '))
 
     while not (0 < jumlah_kendaraan <= int_jumlah_maks - int_jumlah_masuk):
@@ -212,7 +157,6 @@ def subrutin_tambah_kendaraan(int_jumlah_maks, int_jumlah_masuk):
             print('|                                      /_______\\ |')
             print('#-------------------------------------------------#')
         jumlah_kendaraan = int(input('Masukkan jumlah kendaraan: '))
-
     for i in range(jumlah_kendaraan):  # Mulai dari 0
         fungsi_bersihkan_layar()
         print(f'#------------------------------------------#')
@@ -228,24 +172,20 @@ def subrutin_tambah_kendaraan(int_jumlah_maks, int_jumlah_masuk):
 
     return int_jumlah_masuk + jumlah_kendaraan
 
-# subrutin hapus kendaraan
 def subrutin_hapus_kendaraan():
     fungsi_bersihkan_layar()
     print('Hapus Kendaraan')
     input('Tekan Enter untuk Melanjutkan')
 
-# placeholder urutkan kendaraan
 def subrutin_urutkan_kendaraan():
     fungsi_bersihkan_layar()
     print('Urutkan Kendaraan')
     input('Tekan Enter untuk Melanjutkan')
 
-# placeholder cari kendaraan
 def subrutin_cari_kendaraan():
     fungsi_bersihkan_layar()
     print('Cari Kendaraan')
     input('Tekan Enter untuk Melanjutkan')
 
-# DEBUG PANGGIL FUNGSI
 if __name__ == '__main__':
-    subrutin_menu_login(percobaan,batas_percobaan,int_jumlah_masuk)
+    subrutin_menu_login()
