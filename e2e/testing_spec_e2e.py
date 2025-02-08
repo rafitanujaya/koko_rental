590fc03a498afc5000cadc7dfb0e5b68c8b01bdf
wolf33b <wolf33b@duck.com>
2025-02-09 06:45:35 +0700

    feat: fix macro for rafi test and temporary stop macro in problematic area

--- e2e/testing_spec_e2e.py
+++ e2e/testing_spec_e2e.py
@@ -35,22 +35,19 @@ click_list_rafi = [
     "testcar8","botcar8","BK7777BT","4567890123","papi","628574321",
     "testcar9","botcar9","BK8888BT","1234567890","yangmulia","628571234",
     "testcar10","botcar10","BK9999BT","2345678901","pakpol","628574444",
-    "2","5","3","5","1","1","1",
-    "testcartambahan1","botcartambahan1","BK9876BT","4275678321","habibi","62812123456",
+    "2","5","3","5","1","3",
+    "BK9876BT","testcartambahan1","botcartambahan1","4567890123","habibi","62812123456",
     "4","1",
-
+    "BK1234NY","testcartNYISIP1","botcarNYISIP1","4567890123","abu bakar","62812000056",
+    "7","8","6","2","1","5",
+    "testcar1", "botcar1", "BK1111BT", "1234567890", "bob", "628571234",
+    "testcar2", "botcar2", "BK2222BT", "2345678901", "agus", "628574444",
+    "testcar3", "botcar3", "BK3333BT", "3456789012", "budi", "628571212",
+    "testcar4", "botcar4", "BK4444BT", "4567890123", "bangbang", "628574321",
+    "testcar5", "botcar5", "BK1111BT", "1234567890", "emely", "628571234","2"
     ]
 
-'''
-"testcartNYISIP1", "botcarNYISIP1", "BK1234NY", "8422678321", "abu bakar", "62812000056",
-"2", "7", "8", "6", "1", "5",
-"testcar1", "botcar1", "BK1111BT", "1234567890", "bob", "628571234",
-"testcar2", "botcar2", "BK2222BT", "2345678901", "agus", "628574444",
-"testcar3", "botcar3", "BK3333BT", "3456789012", "budi", "628571212",
-"testcar4", "botcar4", "BK4444BT", "4567890123", "bangbang", "628574321",
-"testcar5", "botcar5", "BK1111BT", "1234567890", "emely", "628571234",
-'''
-
+#PLAT/MODEL/BRAND/HARGA/SUPIR/NOSUPIR
 def automate(click_list,delay=0.1,islocked=True,isselectadmin=True):
     print("TEST IN PROGRESS. DO NOT TOUCH!")
     time.sleep(1)
@@ -73,9 +70,10 @@ def automate(click_list,delay=0.1,islocked=True,isselectadmin=True):
     for text_to_insert in click_list:
         if keyboard.is_pressed('backspace'):
             exit("Emergency triggered!")
-        keyboard.write(text_to_insert, delay=0.008)
+        keyboard.write(text_to_insert, delay=0.01)
         keyboard.press_and_release('enter')
         time.sleep(delay)
+    print("TEST PASSED")
 
 
 def main():
