308dcbcf62ea45e1f5964f4798248bab6b0d1961
wolf33b <wolf33b@duck.com>
2025-02-09 00:27:13 +0700

    feat: init

--- e2e/testing_spec_e2e.py
+++ e2e/testing_spec_e2e.py
@@ -1,1 +1,14 @@
-# e2e code
+import mouse
+import keyboard
+import time
+
+print("TEST IN PROGRESS. DO NOT TOUCH!")
+time.sleep(1)
+
+mouse.move(-4000, -4000, absolute=True, duration=0.1)
+mouse.move(500,800, absolute=True, duration=0.1)
+# Click at the current mouse position
+mouse.click()
+
+text_to_insert = "test"
+keyboard.write(text_to_insert, delay=0.01)
