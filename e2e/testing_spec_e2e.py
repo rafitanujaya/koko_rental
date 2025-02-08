07714b08529568fc16289b74ebd185d01013fda3
wolf33b <wolf33b@duck.com>
2025-02-09 01:29:36 +0700

    feat: add macro for fauzi and rafi

--- e2e/testing_spec_e2e.py
+++ e2e/testing_spec_e2e.py
@@ -4,8 +4,8 @@ import time
 
 password="admin123"
 #LIST PAUJI
-#flow unit>menu1>input+>enter>data>show
-click_list_default = [
+#flow unit>menu1>input+>enter>data>show>delete-1>show>
+click_list_pauji = [
     "10", "1", "10","",
     "testcar1","botcar1","BK1111BT","1234567890","bob","628571234",
     "testcar2","botcar2","BK2222BT","2345678901","agus","628574444",
@@ -17,10 +17,23 @@ click_list_default = [
     "testcar8","botcar8","BK7777BT","4567890123","papi","628574321",
     "testcar9","botcar9","BK8888BT","1234567890","yangmulia","628571234",
     "testcar10","botcar10","BK9999BT","2345678901","pakpol","628574444",
-    "7","","2","1","7","",
+    "7","","2","","1","7","","4","1","1"
     ]
 
-click_list = ["4", "5", "6"]
+click_list_rafi = [
+    "1","admin", "1", "1","10",
+    "testcar1","botcar1","BK1111BT","1234567890","bob","628571234",
+    "testcar2","botcar2","BK2222BT","2345678901","agus","628574444",
+    "testcar3","botcar3","BK3333BT","3456789012","budi","628571212",
+    "testcar4","botcar4","BK4444BT","4567890123","bangbang","628574321",
+    "testcar5","botcar5","BK1111BT","1234567890","emely","628571234",
+    "testcar6","botcar6","BK5555BT","2345678901","pauji","628574444",
+    "testcar7","botcar7","BK6666BT","3456789012","edd","628571212",
+    "testcar8","botcar8","BK7777BT","4567890123","papi","628574321",
+    "testcar9","botcar9","BK8888BT","1234567890","yangmulia","628571234",
+    "testcar10","botcar10","BK9999BT","2345678901","pakpol","628574444",
+    "2","5","3",
+    ]
 
 
 
@@ -53,15 +66,15 @@ def main():
     while True:
         try:
             print("""
-            1.automate default
-            2.automate list
+            1.automate pauji
+            2.automate rafi
             3.automate custom
             """)
             menu = int(input(">"))
             if menu == 1:
-                automate(click_list_default,0.01,True,False)
+                automate(click_list_pauji,0.01,True,False)
             elif menu == 2:
-                automate(click_list, 0.01)
+                automate(click_list_rafi, 0.01,False,False)
         except Exception as e:
             print("error:", e)
 
