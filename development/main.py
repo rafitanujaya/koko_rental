d9ce349772ed487a8f3d7daae223b0141614ba45
rafi tanujaya <rafibussines@gmail.com>
2025-02-09 22:40:49 +0700

    ui: adding interface logout and main menu

---
+++ development/main.py
@@ -0,0 +1,64 @@
+# DEKLARASI VARIABEL
+
+# Menu
+menu_utama = 0
+
+
+
+def fungsi_menu_utama(menu_utama):
+    print('╔════════════════════════════════════════════════════════════════════════════╗')
+    print('║                        >> PILIH PERAN ANDA <<                              ║')
+    print('╠════════════════════════════════════════════════════════════════════════════╣')
+    print('║ [1] ADMIN / STAFF                                                          ║')
+    print('║     Kelola unit kendaraan, pengguna, dan data operasional rental.          ║')
+    print('╠────────────────────────────────────────────────────────────────────────────╣')
+    print('║ [2] USER                                                                   ║')
+    print('║     Pesan kendaraan favorit Anda dengan cepat dan mudah!                   ║')
+    print('╠────────────────────────────────────────────────────────────────────────────╣')
+    print('║ [0] KELUAR                                                                 ║')
+    print('║     Selesaikan sesi ini dan kembali nanti.                                 ║')
+    print('╚════════════════════════════════════════════════════════════════════════════╝')
+    print('>>> Masukkan pilihan Anda dan tekan [ENTER] untuk melanjutkan.')
+    input()
+    menu_utama = int(input(' Masukkan pilihan Anda : '))
+    
+    while (menu_utama != 1) and (menu_utama != 2) and (menu_utama != 0):
+        print('╔════════════════════════════════════════════════════════════════════════════╗')
+        print('║                        >> PILIH PERAN ANDA <<                              ║')
+        print('╠════════════════════════════════════════════════════════════════════════════╣')
+        print('║ [1] ADMIN / STAFF                                                          ║')
+        print('║     Kelola unit kendaraan, pengguna, dan data operasional rental.          ║')
+        print('╠────────────────────────────────────────────────────────────────────────────╣')
+        print('║ [2] USER                                                                   ║')
+        print('║     Pesan kendaraan favorit Anda dengan cepat dan mudah!                   ║')
+        print('╠────────────────────────────────────────────────────────────────────────────╣')
+        print('║ [0] KELUAR                                                                 ║')
+        print('║     Selesaikan sesi ini dan kembali nanti.                                 ║')
+        print('╚════════════════════════════════════════════════════════════════════════════╝')
+        print('>>> Masukkan pilihan Anda dan tekan [ENTER] untuk melanjutkan.')
+        input()
+        menu_utama = int(input(' Masukkan pilihan Anda : '))
+    
+    return menu_utama
+
+menu_utama = fungsi_menu_utama(menu_utama)
+while menu_utama != 0:
+    match menu_utama: 
+        case 1:
+            print('a')
+        case 2:
+            print('b')
+    menu_utama = fungsi_menu_utama(menu_utama)
+
+# Keluar aplikasi
+print('╔══════════════════════════════════════════════════════════════════════════╗')
+print('║                                                                          ║')
+print('║   +  T E R I M A   K A S I H   T E L A H   M E N G G U N A K A N   +     ║')
+print('║                     +   K O K O   R E N T A L   +                        ║')
+print('║                                                                          ║')
+print('║   Semoga perjalanan Anda menyenangkan dan penuh pengalaman seru!         ║')
+print('║   Jangan ragu untuk kembali menggunakan layanan kami kapan saja.         ║')
+print('║                                                                          ║')
+print('║   Follow kami di @KokoRental untuk promo menarik dan info terbaru!       ║')
+print('║                                                                          ║')
+print('╚══════════════════════════════════════════════════════════════════════════╝')
